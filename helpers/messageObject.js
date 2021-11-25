c64c16a32283bd4356c9cd54aacecee0828c675d
pimby <baraka608@student.mmarau.ac.ke>
2021-11-25 12:49:59 +0300

    added cc evans

--- helpers/messageObject.js
+++ helpers/messageObject.js
@@ -1,23 +1,23 @@
 const content = require("./calenderObject");
 
 const messageObj = {
-    from: {
-        name: 'caleb kellah',
-        address: `${process.env.EMAIL_USER}`
-    },
-    to: 'baraka608@student.mmarau.ac.ke',
-    subject: "Friends and colleagues get together",
-     html: `<div>
+  from: {
+    name: "caleb kellah",
+    address: `${process.env.EMAIL_USER}`,
+  },
+  to: "baraka608@student.mmarau.ac.ke",
+  cc: "evans.kibet@thejitu.com",
+  subject: "Friends and colleagues get together",
+  html: `<div>
                 <h4 style="text-align: center">Friends and colleagues get together</h4>
                 <p style="text-align: center; color: grey">Come lets have fun</p>
                 <p style="text-align: center; color:grey;">Bring along your friends</p>
                 <p style="text-align: center;color:grey">Meet new people and make new friends</p>
                 
        </div>`,
-     icalEvent: {
-        content: content
-      },
-  
-}
+  icalEvent: {
+    content: content,
+  },
+};
 
-module.exports = messageObj
+module.exports = messageObj;
